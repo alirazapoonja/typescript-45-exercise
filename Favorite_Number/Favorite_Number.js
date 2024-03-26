2a74743e05e3aac3211266d8ef0415fd9c26252a
alirazapoonja <160298055+alirazapoonja@users.noreply.github.com>
2024-03-27 04:38:24 +0500

    Add files via upload

---
+++ Favorite_Number/Favorite_Number.js
@@ -0,0 +1,4 @@
+"use strict";
+const favorite_number = 6;
+const message = 'Here is your favoriate number ' + favorite_number;
+console.log(message);
---
+++ Favorite_Number/Favorite_Number.ts
@@ -0,0 +1,3 @@
+const favorite_number :number = 6; 
+const message : string = 'Here is your favoriate number ' + favorite_number ; 
+console.log(message);
