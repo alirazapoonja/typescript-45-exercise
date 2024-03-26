1b0e21bbc04a57f51d3a9e2f01ddccba1daac4f0
alirazapoonja <160298055+alirazapoonja@users.noreply.github.com>
2024-03-27 04:54:24 +0500

    Add files via upload

---
+++ International_Error/International_Error.js
@@ -0,0 +1,4 @@
+"use strict";
+let newArray = ['will smith', 'James Bond', 'Tom Cruise'];
+console.log(newArray[5]);
+console.log(newArray);
---
+++ International_Error/International_Error.ts
@@ -0,0 +1,7 @@
+let newArray :string[] =['will smith', 'James Bond', 'Tom Cruise'];
+
+console.log(newArray[5]);
+
+console.log(newArray);
+
+
