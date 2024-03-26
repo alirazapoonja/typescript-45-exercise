2691f0cbbc0e3c83e5bd47a58a19ba2a954cbbdc
alirazapoonja <160298055+alirazapoonja@users.noreply.github.com>
2024-03-27 04:41:57 +0500

    Add files via upload

---
+++ Greetings/Names.ts
@@ -0,0 +1,6 @@
+let names : string[] = ['Talha', 'Asad', 'Ali', 'Raza', 'Sultan'];
+
+const message : string = 'Welcome to Typescript class:'
+for(let i=0; i<names.length; i++){
+    console.log(message + names[i]);
+}
