826dd8ea3a0c98491a704644bb0f0d93dfa82abd
alirazapoonja <160298055+alirazapoonja@users.noreply.github.com>
2024-03-27 04:53:18 +0500

    question number 21
    question number 21

---
+++ Typescript_Objects/typescript_objects.js
@@ -0,0 +1,10 @@
+"use strict";
+// let person_name :string = 'Taha';
+// const personname_Array :string [] = ['Ahmed', 'Bilal', 'Hira'];
+let person = {
+    age: 26,
+    name: 'Taha',
+    nationality: 'Pakistan',
+    Student: true
+};
+console.log(person);
---
+++ Typescript_Objects/typescript_objects.ts
@@ -0,0 +1,19 @@
+// let person_name :string = 'Taha';
+// const personname_Array :string [] = ['Ahmed', 'Bilal', 'Hira'];
+
+interface person {
+    age : number, 
+    name : string,
+    nationality : string,
+    Student : Boolean
+    }
+
+let person :person = {
+    
+    age : 26 , 
+    name : 'Taha',
+    nationality : 'Pakistan',
+    Student : true
+}
+console.log(person);
+
