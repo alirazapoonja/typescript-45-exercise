a5deced0829acd06ffc6aa3fbbbbba325d81f17b
alirazapoonja <160298055+alirazapoonja@users.noreply.github.com>
2024-03-27 04:34:47 +0500

    Add files via upload

---
+++ Famous_Quote_2/Famous_Quote_2.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let famous_person = 'Albert Einstein';
+let message = '"A person who never made a mistake never tried anything new"';
+console.log(`${famous_person} once said, ${message}`);
