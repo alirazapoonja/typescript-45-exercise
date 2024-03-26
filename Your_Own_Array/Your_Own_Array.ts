8b3c1b83c39f637022819ec194d586d2cf945638
alirazapoonja <160298055+alirazapoonja@users.noreply.github.com>
2024-03-27 04:42:58 +0500

    Add files via upload

---
+++ Your_Own_Array/Your_Own_Array.ts
@@ -0,0 +1,6 @@
+
+let transportation : string[] = ['motorcyle', 'Car', 'Train', 'Cycle'];
+for(let i=0; i<transportation.length; i++){
+console.log('I would like to own a' + transportation [i])
+
+}
