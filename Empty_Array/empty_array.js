c50cd1bc094080ba0ea30c2352731019003aa118
alirazapoonja <160298055+alirazapoonja@users.noreply.github.com>
2024-03-27 04:51:58 +0500

    question number 20
    question number 20

---
+++ Empty_Array/empty_array.js
@@ -0,0 +1,7 @@
+"use strict";
+let items = ['Pakistan', 'Karachi', 'Urdu'];
+let items2 = [];
+items2.push('Germany');
+items2.push('England');
+items2.push('India');
+console.log(items2);
---
+++ Empty_Array/empty_array.ts
@@ -0,0 +1,10 @@
+let items :string[] = ['Pakistan', 'Karachi', 'Urdu'];
+
+let items2 : string[] = [];
+
+items2.push('Germany');
+items2.push('England');
+items2.push('India');
+
+console.log(items2);
+
