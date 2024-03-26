a2a476df2e38441bfa5fdcca22a6bbe1a8672bb3
alirazapoonja <160298055+alirazapoonja@users.noreply.github.com>
2024-03-27 04:44:11 +0500

    Add files via upload

---
+++ Guest_List/Guest_Lists.js
@@ -0,0 +1,5 @@
+"use strict";
+let Guest_List = ['Ali Raza', 'Imran Ali', 'Junaid',];
+for (let i = 0; i < Guest_List.length; i++) {
+    console.log('Dear Mr. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite your in our party.\n\nThank You!');
+}
---
+++ Guest_List/Guest_Lists.ts
@@ -0,0 +1,4 @@
+let Guest_List : string[] = ['Ali Raza', 'Imran Ali', 'Junaid',];
+for(let i=0; i<Guest_List.length; i++){
+    console.log('Dear Mr. ' + Guest_List [i]+ ',\n\nIt is our pleasure to invite your in our party.\n\nThank You!')
+}
