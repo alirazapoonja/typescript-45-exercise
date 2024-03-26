7acf985d1de03e5ed008595d3f3357e3a8aacff9
alirazapoonja <160298055+alirazapoonja@users.noreply.github.com>
2024-03-27 04:59:34 +0500

    Add files via upload

---
+++ Alien_Colors/Alien_colors.ts
@@ -0,0 +1,15 @@
+
+let alien_color : string = 'green';
+
+if(alien_color === 'green'){
+    console.log("Player Just earned 5 points !");
+ 
+}
+
+alien_color = 'yellow';
+ 
+if(alien_color === 'green'){
+    console.log("Player Just earned 5 points !");
+    
+}
+    
