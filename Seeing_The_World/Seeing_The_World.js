c1c2437fd962247ed54e783f8256c8a44691906b
alirazapoonja <160298055+alirazapoonja@users.noreply.github.com>
2024-03-27 04:49:09 +0500

    Add files via upload

---
+++ Seeing_The_World/Seeing_The_World.js
@@ -0,0 +1,12 @@
+"use strict";
+let places = ['Itlay', 'Germany', 'Agra', 'Eifel Tower', 'Times Square',];
+console.log('Original ' + places);
+console.log('copy ' + [...places].sort());
+console.log('Original ' + places);
+console.log('copy ' + [...places].sort().reverse());
+console.log('Original ' + places);
+console.log('copy ' + [...places].sort().reverse());
+console.log('Original ' + places);
+console.log('Original ' + places.reverse());
+console.log('Original ' + places.reverse());
+console.log('Original ' + places.sort().reverse());
---
+++ Seeing_The_World/Seeing_The_World.ts
@@ -0,0 +1,21 @@
+let places :String[] = ['Itlay', 'Germany', 'Agra', 'Eifel Tower', 'Times Square',];
+
+console.log('Original ' + places);
+
+console.log('copy ' + [...places].sort());
+
+console.log('Original ' + places);
+
+console.log('copy ' + [...places].sort().reverse());
+
+console.log('Original ' + places);
+
+console.log('copy ' + [...places].sort().reverse());
+
+console.log('Original ' + places);
+
+console.log('Original ' + places.reverse());
+
+console.log('Original ' + places.reverse());
+
+console.log('Original ' + places.sort().reverse());
