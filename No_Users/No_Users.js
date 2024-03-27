b8ff7b3d01e9758120ccd3fa13d7b6574781752b
alirazapoonja <160298055+alirazapoonja@users.noreply.github.com>
2024-03-27 05:08:09 +0500

    Add files via upload

---
+++ No_Users/No_Users.js
@@ -0,0 +1,9 @@
+"use strict";
+let usernames = ['admin', 'Junaid', 'Zeeshan', 'Kamran', 'Daniyal'];
+if (usernames.length === 0) {
+    console.log('we need to find some users!');
+}
+else {
+    usernames = [];
+    console.log('Ali users Names has been removed. ' + usernames.length);
+}
