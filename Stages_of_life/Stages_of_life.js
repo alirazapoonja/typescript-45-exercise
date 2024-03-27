e92517d61ac9b17c83922d5d7503d17b25703aa8
alirazapoonja <160298055+alirazapoonja@users.noreply.github.com>
2024-03-27 05:02:12 +0500

    Add files via upload

---
+++ Stages_of_life/Stages_of_life.js
@@ -0,0 +1,23 @@
+"use strict";
+let age = 25;
+if (age < 2) {
+    console.log("person is a baby");
+}
+else if (age >= 2 && age < 4) {
+    console.log("person is a toddler.");
+}
+else if (age >= 4 && age < 13) {
+    console.log("person is a toddler.");
+}
+else if (age >= 13 && age < 20) {
+    console.log("person is a teenage.");
+}
+else if (age >= 20 && age < 65) {
+    console.log("person is a adult.");
+}
+else if (age >= 65 && age < 13) {
+    console.log("person is a toddler.");
+}
+else {
+    console.log("person is elder");
+}
