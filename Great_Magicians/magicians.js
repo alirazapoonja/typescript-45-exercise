ee3615555de5b08472580622ecfa7d13135c4f14
alirazapoonja <160298055+alirazapoonja@users.noreply.github.com>
2024-03-27 05:23:57 +0500

    Add files via upload

---
+++ Great_Magicians/magicians.js
@@ -0,0 +1,14 @@
+"use strict";
+let magician = ['Harry potter', 'hermione Granger', 'Ron Weasley', 'Albus Dumbledore'];
+function make_great(magicianArry) {
+    for (let i = 0; i < magicianArry.length; i++) {
+        magician[i] = 'the Great ' + magicianArry[i];
+    }
+}
+function show_magicians(magicians) {
+    magicians.forEach(element => {
+        console.log(element);
+    });
+}
+make_great(magician);
+show_magicians(magician);
