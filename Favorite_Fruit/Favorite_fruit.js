0d50897462840ede9875e1989a4e9686bd81cfcb
alirazapoonja <160298055+alirazapoonja@users.noreply.github.com>
2024-03-27 05:03:13 +0500

    Add files via upload

---
+++ Favorite_Fruit/Favorite_fruit.js
@@ -0,0 +1,17 @@
+"use strict";
+let favorite_fruits = ['Apple', 'Banana', 'Mango'];
+if (favorite_fruits.includes('Apple')) {
+    console.log('I really like apple!');
+}
+if (favorite_fruits.includes('Banana')) {
+    console.log('I really like Banana!');
+}
+if (favorite_fruits.includes('Mango')) {
+    console.log('I really like Mango!');
+}
+if (favorite_fruits.includes('Strawberry')) {
+    console.log('I really like Strawberry!');
+}
+if (favorite_fruits.includes('Orange')) {
+    console.log('I really like Orange!');
+}
---
+++ Favorite_Fruit/Favorite_fruit.ts
@@ -0,0 +1,23 @@
+
+let favorite_fruits: string[] = ['Apple', 'Banana', 'Mango'];
+
+if(favorite_fruits.includes ('Apple')){
+console.log ('I really like apple!');
+}
+
+if(favorite_fruits.includes ('Banana')){
+    console.log ('I really like Banana!');
+    }
+
+    if(favorite_fruits.includes ('Mango')){
+        console.log ('I really like Mango!');
+        }
+
+        if(favorite_fruits.includes ('Strawberry')){
+            console.log ('I really like Strawberry!');
+            }   
+
+            if(favorite_fruits.includes ('Orange')){
+                console.log ('I really like Orange!');
+                } 
+
