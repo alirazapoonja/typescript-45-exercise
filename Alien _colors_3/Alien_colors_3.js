0e485c09036db4310cf50fc60488689e9fcf8df1
alirazapoonja <160298055+alirazapoonja@users.noreply.github.com>
2024-03-27 05:01:05 +0500

    Add files via upload

---
+++ Alien _colors_3/Alien_colors_3.js
@@ -0,0 +1,40 @@
+"use strict";
+let aliencolors = "green";
+if (aliencolors === "green") {
+    console.log("version 1: player earned 5 points.");
+}
+else if (aliencolors = "yellow") {
+    console.log("player earned 10 points.");
+}
+else if (aliencolors = "red") {
+    console.log("player earned 15 points.");
+}
+else {
+    console.log("please select right color");
+}
+aliencolors = "yellow";
+if (aliencolors === "green") {
+    console.log("player earned 5 points.");
+}
+else if (aliencolors === "yellow") {
+    console.log("version 2: player earned 10 points.");
+}
+else if (aliencolors === "red") {
+    console.log("player earned 15 points.");
+}
+else {
+    console.log("please select right color");
+}
+aliencolors = "red";
+if (aliencolors === "green") {
+    console.log("player earned 5 points.");
+}
+else if (aliencolors === "yellow") {
+    console.log("player earned 10 points.");
+}
+else if (aliencolors === "red") {
+    console.log("version 3: player earned 15 points.");
+}
+else {
+    console.log("please select right color");
+}
