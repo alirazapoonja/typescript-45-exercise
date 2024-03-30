f8abd59950481b9b82e47b3db890514b5e978662
alirazapoonja <160298055+alirazapoonja@users.noreply.github.com>
2024-03-31 00:24:04 +0500

    Delete Album/Animals.js

--- Album/Animals.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-let Animals = ['dog', 'cat', 'rabbit'];
-for (let Animal of Animals) {
-    console.log(`A ${Animal} would make a great pet.`);
-}
-console.log(`Any of these animals would make a great pet!`);
