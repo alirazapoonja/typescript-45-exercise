7ba819ad36778ca2b85f49e81485964b95623511
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-04-03 04:58:53 +0500

    Add files via upload

---
+++ Large_Shirts/large_shirts.ts
@@ -0,0 +1,13 @@
+
+
+function make_shirt(size:string = 'Large', text:string = 'I love Typescript'){
+    console.log (`Creating a ${size} shirt with the message: ${text}`);
+
+}
+
+make_shirt();
+
+make_shirt('Medium');
+
+make_shirt('Small', 'I love Python');
+
