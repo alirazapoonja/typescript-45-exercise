88b3535f42f381c60839ebfacc3d0a41a9b531d2
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-04-03 04:48:18 +0500

    Add files via upload

---
+++ Animals/Animals.ts
@@ -0,0 +1,11 @@
+
+
+let Animals: string [] = ['dog', 'cat', 'rabbit'];
+
+for (let Animal of Animals){
+
+    console.log (`A ${Animal} would make a great pet.`);
+}
+
+    console.log (`Any of these animals would make a great pet!`);
+
