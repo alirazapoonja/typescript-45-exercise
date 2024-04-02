632b21f891fe5f6910c93543c4a11e8f53c86572
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-04-03 04:50:33 +0500

    Add files via upload

---
+++ Cities/cities.ts
@@ -0,0 +1,15 @@
+
+
+function describe_city(city: string, country: string = 'Default country.'){
+
+    console.log (` ${city} is in ${country}`);
+   
+}
+
+describe_city ('Karachi', 'Pakistan');
+
+describe_city ('Tokyo', 'Japan');
+
+describe_city ('Paris',);
+
+
