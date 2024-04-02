1059b9e1bc3da9117c6621daf099845ee4d63bb1
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-04-03 04:59:28 +0500

    Add files via upload

---
+++ Magicians/magicians.ts
@@ -0,0 +1,12 @@
+
+
+let magician : string [] = ['Harry potter', 'hermione Granger', 'Ron Weasley', 'Albus Dumbledore'];
+
+function show_magicians(magicians: string[]){
+
+    magicians.forEach(element => {
+        console.log (element);
+    });
+
+}
+show_magicians (magician); 
