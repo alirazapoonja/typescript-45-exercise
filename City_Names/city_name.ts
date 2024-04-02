43894f264e5dc0106ea7f0c0f04680f56488360b
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-04-03 04:51:04 +0500

    Add files via upload

---
+++ City_Names/city_name.ts
@@ -0,0 +1,13 @@
+
+
+function city_country(city: string, country: string){
+
+    return `${city} ${country}`;
+    
+}
+
+console.log (city_country ('Karachi', 'Pakistan'));
+
+console.log (city_country ('Tokyo', 'Japan'));
+
+console.log (city_country ('Paris', 'France'));
