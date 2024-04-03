af3b54e10116b97a05f6f5175380d8d67d81d21a
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-04-03 05:02:23 +0500

    Add files via upload

---
+++ Pizzas/pizzas.ts
@@ -0,0 +1,11 @@
+
+let Pizzas: string [] = ['Pepperoni', 'Cuban', 'BBQ chicken'];
+
+for(let Pizza of Pizzas ){
+
+    console.log (`i like ${Pizza} pizza.`);
+   
+}
+
+console.log(`I really love pizza!`);
+
