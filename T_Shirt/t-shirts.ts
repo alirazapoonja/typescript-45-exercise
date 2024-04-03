3c9063668c5bb37594f32aca1a462dd5aa642196
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-04-03 05:04:22 +0500

    Add files via upload

---
+++ T_Shirt/t-shirts.ts
@@ -0,0 +1,11 @@
+
+function make_shirt(size:string, text:string ){
+    console.log (`Creating a ${size} shirt with the message: ${text}`);
+
+}
+
+make_shirt('Large', 'Hello world');
+
+make_shirt('Medium', 'Hello world');
+
+make_shirt('Small', 'Hello world');
