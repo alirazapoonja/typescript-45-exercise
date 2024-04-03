3a728b2e5af329a0c213cc74884443c6bab2febf
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-04-03 05:03:01 +0500

    Add files via upload

---
+++ Sandwiches/sandwiches.ts
@@ -0,0 +1,16 @@
+
+
+function makeSandwich (item: string []){
+console.log('\nMaking your sandwich with:');
+
+item.forEach (element => console.log ("- " + element));
+console.log ('Enjoy your sandwich !\n');
+
+}
+
+makeSandwich(['Ham', 'Cheese', 'lettuce']);
+
+makeSandwich(['Turkey', 'Bacon']);
+
+makeSandwich(['Peanut butter', 'Jelly',]);
+
